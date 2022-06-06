198453a794499d81e9d654432d37df96d01679d6
varma978 <105624137+varma978@users.noreply.github.com>
2022-06-06 22:57:43 +0530

    done my work

---
+++ jdbcapp4/src/com/skill/jdbcapp/utility/Utility.java
@@ -0,0 +1,20 @@
+package com.skill.jdbcapp.utility;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class Utility {
+	public static void getConnection() {
+		
+	Connection con = null;
+	try {
+		FileReader fr= new FileReader();
+		Properties p=new Properties();
+		p.setProperty(1, )
+	}
+		
+		
+	}
+
+}
